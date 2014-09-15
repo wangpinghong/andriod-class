148d56b10bb2981b3f9ef9613cd5cdd11e4054ce
wangpinghong <johnny83721@gmail.com>
2014-09-15 21:07:33 +0800

    SimpleUI ver.5
    add writefile method
    Fileoutput

--- SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -1,8 +1,12 @@
 package com.example.simpleui;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 public class MessageActivity extends Activity {
@@ -17,5 +21,20 @@ protected void onCreate(Bundle savedInstanceState) {
 	
 	String text=getIntent().getStringExtra("text");
 	textView.setText(text);
+}
+
+private void writefile (String text){
+	try {
+		FileOutputStream fos = openFileOutput("History", Context.MODE_APPEND);
+		fos.write(text.getBytes());
+		fos.close();
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
 }
 }
