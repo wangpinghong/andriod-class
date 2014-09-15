2e1c44fbafa56b22c759fe3ab01bae8451bffb45
wangpinghong <johnny83721@gmail.com>
2014-09-15 20:22:54 +0800

    SimpleUI ver.3
    add intent (MessageActivity.class)
    sp (edittext,checkbox)

---
+++ SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -0,0 +1,14 @@
+package com.example.simpleui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MessageActivity extends Activity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	Log.d("debug", "MessaageActivity");
+}
+}
