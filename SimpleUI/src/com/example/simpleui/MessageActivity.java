bba0f752813b3c4329e768acc077b348fd422544
wangpinghong <johnny83721@gmail.com>
2014-09-15 20:39:05 +0800

    SimpleUI ver.4
    add MessageActivity.class
    getIntent from MainActivity,sendtext to MessageActivity

--- SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -3,12 +3,19 @@ package com.example.simpleui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MessageActivity extends Activity {
-@Override
+
+private TextView textView;
+
 protected void onCreate(Bundle savedInstanceState) {
 	// TODO Auto-generated method stub
+	textView=(TextView)findViewById(R.id.textView1);
+	
 	super.onCreate(savedInstanceState);
-	Log.d("debug", "MessaageActivity");
+	
+	String text=getIntent().getStringExtra("text");
+	textView.setText(text);
 }
 }
